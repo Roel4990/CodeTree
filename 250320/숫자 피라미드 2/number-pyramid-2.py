801b32f81e0e59367944eb6d2bd7f83024d40fe6
An Sehong <ash49901@gmail.com>
2025-03-20 14:00:13 +0900

    Title: 숫자 피라미드 2, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250320/숫자 피라미드 2/number-pyramid-2.py
@@ -0,0 +1,12 @@
+# start, end = map(int, input().split())
+
+# # Please write your code here.
+N = int(input())
+
+start = 1
+
+for i in range(1, N+1):
+    for j in range(i):
+        print(start, end=" ")
+        start += 1
+    print()
