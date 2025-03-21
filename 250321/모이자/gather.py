488ed35c4a3e55342b6e0d57da0e24ef502be535
An Sehong <ash49901@gmail.com>
2025-03-21 23:26:50 +0900

    Title: 모이자, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/모이자/gather.py
+++ 250321/모이자/gather.py
@@ -4,7 +4,7 @@ A = list(map(int, input().split()))
 # Please write your code here.
 import sys
 min_val = sys.maxsize
-for i in range(5):
+for i in range(n):
     move = 0
     for j in range(n):
         move += A[j] * abs(j - i)
