6c5e9b2f1a705d68ca89ecffa9158915b52d8260
An Sehong <ash49901@gmail.com>
2025-03-23 16:49:24 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250323/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
+++ 250323/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -3,7 +3,8 @@ n = int(input())
 # Please write your code here.
 # for i in range(n, -n-1, -1):
 #     if i == 0: continue
-#     print("* " * i)
+#     print("* " * abs(i))
+
 def stars(N):
     if N == 0:
         return
