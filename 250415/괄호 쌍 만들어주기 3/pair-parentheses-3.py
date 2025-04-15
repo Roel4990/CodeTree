771654c6142c942f748141bef0993d85137b27c7
An Sehong <ash49901@gmail.com>
2025-04-15 11:53:16 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250415/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 250415/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -4,7 +4,7 @@ A = input()
 cnt = 0
 for i in range(len(A)):
     for j in range(len(A)):
-        if A[i] != ")" and i < j and A[j] == ")":
+        if A[i] != ")" and A[j] == ")":
             cnt += 1
 
 print(cnt)
