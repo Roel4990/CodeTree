987624e82706ad06684d1993523b26cc1dde0f0a
An Sehong <ash49901@gmail.com>
2025-04-15 11:53:47 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250415/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 250415/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -2,9 +2,9 @@ A = input()
 
 # Please write your code here.
 cnt = 0
-for i in range(len(A)):
-    for j in range(len(A)):
-        if A[i] != ")" and i < j and A[j] == ")":
+for i in range(n):
+    for j in range(i + 1, n):
+        if string[i] == '(' and string[j] == ')':
             cnt += 1
 
 print(cnt)
