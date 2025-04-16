a522e1b1e3171e2db65314b6b6d6d6e679d57715
An Sehong <ash49901@gmail.com>
2025-04-17 02:59:25 +0900

    Title: 전부 겹치는 선분, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250416/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,9 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+x1, x2 = zip(*segments)
+x1, x2 = list(x1), list(x2)
+
+# Please write your code here.
+
+print("Yes" if max(x1) <= min(x2) else "No")
+    
