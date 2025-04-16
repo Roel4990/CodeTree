13c74eb9154949c64de4aa96e58e4194af26997c
An Sehong <ash49901@gmail.com>
2025-04-16 21:31:34 +0900

    Title: 함수를 이용한 소수 판별, Time: 50ms, Memory: 19MB, Status: Passed - Codetree

--- 250416/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250416/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -5,6 +5,8 @@ a, b = map(int, input().split())
 def f(a, b):
     answerList = []
     for i in range(a, b+1):
+        if i < 2:
+            continue
         isPrimeNumber = True
         for j in range(2, i):
             if i % j == 0:
