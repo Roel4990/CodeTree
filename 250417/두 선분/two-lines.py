e7c7d16ada26fad3c23ae55faf79fa287e1e71c1
An Sehong <ash49901@gmail.com>
2025-04-17 02:26:54 +0900

    Title: 두 선분, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 250417/두 선분/two-lines.py
+++ 250417/두 선분/two-lines.py
@@ -2,13 +2,9 @@ x1, x2, x3, x4 = map(int, input().split())
 
 # Please write your code here.
 
-if x3 <= x1 <= x4:
-    print("intersecting")
-elif x3 <= x2 <= x4:
-    print("intersecting")
-elif x1 <= x3 <= x2:
-    print("intersecting")
-elif x1 <= x4 <= x2:
+if x3 <= x2 and x1 <= x4:
+    print('intersecting')
+elif x2 <= x3 and x4 <= x1:
     print("intersecting") 
 else:
     print("nonintersecting")
