46f9442df97af40ba963e68581bec524a581d946
An Sehong <ash49901@gmail.com>
2025-04-17 02:46:48 +0900

    Title: 구역 청소, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250417/구역 청소/district-cleaning.py
+++ 250417/구역 청소/district-cleaning.py
@@ -2,7 +2,7 @@ a, b = map(int, input().split())
 c, d = map(int, input().split())
 
 # Please write your code here.
-if b > c:
+if b > c and a < d:
     print(max(a,b,c,d) - min(a,b,c,d))
 else:
     print(b-a+d-c)
