52d6a707c229b758e19b1ae11cf25cfec2718ce8
An Sehong <ash49901@gmail.com>
2025-04-18 23:15:25 +0900

    Title: 아름다운 수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250418/아름다운 수/beautiful-number.py
+++ 250418/아름다운 수/beautiful-number.py
@@ -1,28 +1,19 @@
 n = int(input())
 
 # Please write your code here.
-result = []
 cnt = 0
 
-def is_beautiful(s):
-    i = 0
-    while i < len(s):
-        num = int(s[i])
-        if i + num > len(s):
-            return False
-        if s[i:i+num] != s[i] * num:
-            return False
-        i += num
-    return i == len(s)
-
-def recur(current):
+def recur(current, length):
     global cnt
-    if len(current) == n:
-        if is_beautiful(current):
-            cnt += 1
+    if length == n:
+        cnt += 1
         return
-    for i in range(1, 10):  # 1부터 9까지 사용
-        recur(current + str(i))
+    if length > n:
+        return
 
-recur('')
+    for i in range(1, 10):
+        group = str(i) * i
+        recur(current + group, length + i)
+
+recur('', 0)
 print(cnt)
