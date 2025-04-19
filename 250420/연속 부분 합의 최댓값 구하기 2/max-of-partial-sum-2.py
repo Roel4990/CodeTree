2f38abc984507a70ec52f0a107bb97c17a807431
An Sehong <ash49901@gmail.com>
2025-04-20 00:45:29 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 58ms, Memory: 18MB, Status: Passed - Codetree

--- 250420/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 250420/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -1,24 +1,13 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-# Please write your code here.
-answer = min(a)
-first = 0
-second = 0
+def max_subarray_sum(arr):
+    max_sum = current_sum = arr[0]
 
-while True:
-    sumNum = sum(a[first:second+1])
-    answer = max(answer, sumNum)
-    if sumNum > 0:
-        if second == len(a) - 1:
-            break
-        second += 1
-    else:
-        if first == second:
-            if second == len(a) - 1:
-                break
-            first += 1
-            second += 1
-        else:
-            first = second
-print(answer)
+    for i in range(1, len(arr)):
+        current_sum = max(arr[i], current_sum + arr[i])
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+        
+print(max_subarray_sum(a))
