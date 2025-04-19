29caceae23cf59d32d643b2bb3113979174da4ea
An Sehong <ash49901@gmail.com>
2025-04-20 01:37:42 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250420/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,10 @@
+N = int(input())
+nList = list(map(int, input().split()))
+
+cnt = 0
+for i in range(len(nList)):
+    if nList[i] == 2:
+        cnt += 1
+        if cnt == 3:
+            print(i+1)
+            break
