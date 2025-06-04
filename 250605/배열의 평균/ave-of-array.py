485b3a454cff9fee292e806e69ffff60ed377e84
An Sehong <ash49901@gmail.com>
2025-06-05 01:45:44 +0900

    Title: 배열의 평균, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250605/배열의 평균/ave-of-array.py
+++ 250605/배열의 평균/ave-of-array.py
@@ -1,12 +1,13 @@
 numList = []
-
+def f(n):
+    return format(n, '.1f')
 for _ in range(2):
     nList = list(map(int, input().split()))
     numList.append(nList)
-    print(sum(nList)/len(nList), end=" ")
+    print(f(sum(nList)/len(nList)), end=" ")
 print()
 for i in range(4):
-    print((numList[0][i] + numList[1][i])/len(numList), end=" ")
+    print(f((numList[0][i] + numList[1][i])/len(numList)), end=" ")
 print()
-print((sum(numList[0]) + sum(numList[1]))/(len(numList[0]) + len(numList[1])))
+print(f((sum(numList[0]) + sum(numList[1]))/(len(numList[0]) + len(numList[1]))))
 
