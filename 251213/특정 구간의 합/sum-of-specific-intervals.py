f34b23d87ba6cc67a1b599319c99b7ed6c408c2d
An Sehong <ash49901@gmail.com>
2025-12-13 13:54:23 +0900

    Title: 특정 구간의 합, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251213/특정 구간의 합/sum-of-specific-intervals.py
+++ 251213/특정 구간의 합/sum-of-specific-intervals.py
@@ -4,8 +4,5 @@ queries = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
 for query in queries:
-    # if query[1] == len(arr):
-    #     print(sum(arr[query[0]-1:]))
-    # else:
     print(sum(arr[query[0]-1: query[1]]))
     
