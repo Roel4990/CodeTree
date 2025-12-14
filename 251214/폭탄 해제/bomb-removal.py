a3b9dc7eada7a9b1e22d4c3d6ea010581c7c9a26
An Sehong <ash49901@gmail.com>
2025-12-14 23:33:16 +0900

    Title: 폭탄 해제, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251214/폭탄 해제/bomb-removal.py
+++ 251214/폭탄 해제/bomb-removal.py
@@ -1,3 +1,11 @@
 code, color, second = input().split()
 
-print(f"code : {code}\ncolor : {color}\nsecond : {second}")
+class Bomb:
+    def __init__(self, code, color, second):
+        self.code = code
+        self.color = color
+        self.second = second
+
+bomb1 = Bomb(code, color, second)
+
+print(f"code : {bomb1.code}\ncolor : {bomb1.color}\nsecond : {bomb1.second}")
