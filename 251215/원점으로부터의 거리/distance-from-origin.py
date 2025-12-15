4318953e22da811199e62a290bd37481b1f1fed8
An Sehong <ash49901@gmail.com>
2025-12-15 21:07:02 +0900

    Title: 원점으로부터의 거리, Time: 74ms, Memory: 17MB, Status: Passed - Codetree

--- 251215/원점으로부터의 거리/distance-from-origin.py
+++ 251215/원점으로부터의 거리/distance-from-origin.py
@@ -3,15 +3,14 @@ points = [(int(i), tuple(map(int, input().split()))) for i in range(n)]
 
 # Please write your code here.
 class Point:
-    def __init__(self, idx, x, y, distance):
+    def __init__(self, idx, x, y):
         self.idx = idx
         self.x = x
         self.y = y
-        self.distance = distance
 
-points_c = [Point(point[0], point[1][0], point[1][1], abs(point[1][0]) + abs(point[1][1])) for point in points]
+points_c = [Point(point[0], point[1][0], point[1][1]) for point in points]
 
-points_c.sort(key = lambda x: x.distance)
+points_c.sort(key = lambda x: abs(x.x) + abs(x.y))
 
 for point in points_c:
     print(point.idx + 1)
