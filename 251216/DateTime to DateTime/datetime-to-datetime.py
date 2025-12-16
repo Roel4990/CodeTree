9fce94700efc3bcf8a0db425bf063df156517e96
An Sehong <ash49901@gmail.com>
2025-12-16 15:18:21 +0900

    Title: DateTime to DateTime, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251216/DateTime to DateTime/datetime-to-datetime.py
+++ 251216/DateTime to DateTime/datetime-to-datetime.py
@@ -1,7 +1,16 @@
 a, b, c = map(int, input().split())
 
 # Please write your code here.
-if a - 11 < 0:
+START_MIN = (11 * 60) + 11  
+
+end_days_in_min = (a - 11) * 1440
+end_time_in_min = (b * 60) + c
+
+end_total_min = end_days_in_min + end_time_in_min
+
+total_minutes = end_total_min - START_MIN
+
+if total_minutes < 0:
     print(-1)
 else:
-    print((a - 11 - 1) * 24 * 60 + (b + 23 - 11) * 60 + 60 + c - 11)
+    print(total_minutes)
