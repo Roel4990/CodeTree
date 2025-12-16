63bdccef773341de8290d799c6cfe18e62d39113
An Sehong <ash49901@gmail.com>
2025-12-16 15:15:53 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251216/DateTime to DateTime/datetime-to-datetime.py
+++ 251216/DateTime to DateTime/datetime-to-datetime.py
@@ -1,22 +1,7 @@
 a, b, c = map(int, input().split())
 
 # Please write your code here.
-result = 0
-day = 11
-hour = 11
-minute = 11
-while True:
-    if day == a and hour == b and minute == c:
-        break
-
-    result += 1
-    minute += 1
-
-    if minute == 60:
-        hour += 1
-        minute = 0
-    
-    if hour == 24:
-        day += 1
-        hour = 0
-print(result)
+if a - 11 < 0:
+    print(-1)
+else:
+    print((a - 11 - 1) * 24 * 60 + (b + 23 - 11) * 60 + 60 + c - 11)
