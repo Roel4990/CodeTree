d2880ca4c84fe1cd69771beb2f4697df5982dac7
An Sehong <ash49901@gmail.com>
2025-12-17 23:36:37 +0900

    Title: 그 요일은, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251217/그 요일은/the-day-of-the-day.py
+++ 251217/그 요일은/the-day-of-the-day.py
@@ -8,15 +8,18 @@ day_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 count = 0
 result = 0
-for i in range(m1, m2):
-    day = d1
-    if i == m1:
-        count += day_of_month[i] - d1
-    else:
-        count += day_of_month[i]
+if m1 == m2 and d1 == d2:
+    print(0)
+else:
+    for i in range(m1, m2):
+        day = d1
+        if i == m1:
+            count += day_of_month[i] - d1
+        else:
+            count += day_of_month[i]
 
-count += d2
+    count += d2
 
-a = day_of_the_week.index(A)
+    a = day_of_the_week.index(A)
 
-print(count//7 + (1 if count % 7 >= a else 0))
+    print(count//7 + (1 if count % 7 >= a else 0))
